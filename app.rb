4136bcfdcc052310cc7be52c7d67a4d05ceac49f
Роман Легун <legun.roman@yandex.ua>
2016-11-03 18:05:02 +0200

    Add Client

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:MyShop.db"
+
+class Client < ActiveRecord::Base
+end
 
 get '/' do
   erb ""
