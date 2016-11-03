2ab5fcbb4190d45b86d333715d400ba0a09c42ac
Роман Легун <legun.roman@yandex.ua>
2016-11-04 01:50:35 +0200

    Add validates

--- app.rb
+++ app.rb
@@ -6,12 +6,32 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:MyShop.db"
 
 class Client < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :datestamp, presence: true
+  validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
   @barbers = Barber.all
+end
+
+get '/' do
   erb :index
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+
+  c = Client.new params[:client]
+  c.save
+
+  erb "dtyj"
+
 end
