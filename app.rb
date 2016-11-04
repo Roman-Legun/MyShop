7929dfed77d33a7ad86979adbbfc12b28c12fcde
Роман Легун <legun.roman@yandex.ua>
2016-11-04 13:54:32 +0200

    Add link my_js.js

--- app.rb
+++ app.rb
@@ -47,3 +47,12 @@ get '/bookings' do
   @clients = Client.order('created_at DESC')
   erb :bookings
 end
+
+get '/client/:id' do
+  @client = Client.find(params[:id])
+  erb :client
+end
+
+get '/about' do
+  erb :about
+end
