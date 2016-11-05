9dd55d1db0fd0d471b573a0d1cdf75254805daca
Роман Легун <legun.roman@yandex.ua>
2016-11-05 15:58:11 +0200

    Add function cart_get()

--- public/js/my_js.js
+++ public/js/my_js.js
@@ -7,3 +7,27 @@ function xxx(){
 	alert(x);
 
 }
+
+function cart(id){
+
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+	alert('замовлено:' + cart_get());
+}
+
+function cart_get(){
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++){
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+
+		if(key.indexOf('product_') == 0){
+			cnt = cnt + value * 1;
+		}
+	}
+	return cnt;
+}
